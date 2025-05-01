47f738d9b2d703a052f732faf229e1dd39b1f5a3
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2025-05-01 16:09:20 -0300

    fix: type prisma

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,5 +1,5 @@
 import { prisma } from "@/lib/prisma";
-import type { Prisma } from "generated/prisma";
+import type { Prisma } from "@prisma/client";
 import type { UsersRepository } from "../users-repository";
 
 export class PrismaUsersRepository implements UsersRepository {

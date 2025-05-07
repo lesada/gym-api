5ac41393d29c727da03303ce39f82a8eb530ec93
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2025-05-07 13:29:28 -0300

    feat: in memory users

--- src/services/register.spec.ts
+++ src/services/register.spec.ts
@@ -1,24 +1,13 @@
+import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository";
 import { compare } from "bcryptjs";
 import { describe, expect, test } from "vitest";
 import { RegisterService } from "./register";
 
 describe("Register Service", () => {
 	test("should hash user password upon registration", async () => {
-		const registerService = new RegisterService({
-			async findByEmail(email) {
-				return null;
-			},
+		const usersRepository = new InMemoryUsersRepository();
 
-			async create(data) {
-				return {
-					id: "user-1",
-					name: data.name,
-					email: data.email,
-					password_hash: data.password_hash,
-					created_at: new Date(),
-				};
-			},
-		});
+		const registerService = new RegisterService(usersRepository);
 
 		const { user } = await registerService.execute({
 			name: "John Doe",

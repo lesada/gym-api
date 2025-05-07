45de00ba5adf6eb6e750885f9d4658ba88acdb91
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2025-05-07 13:33:13 -0300

    test: should register

--- src/services/register.spec.ts
+++ src/services/register.spec.ts
@@ -5,6 +5,19 @@ import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
 import { RegisterService } from "./register";
 
 describe("Register Service", () => {
+	test("should register", async () => {
+		const usersRepository = new InMemoryUsersRepository();
+		const registerUseCase = new RegisterService(usersRepository);
+
+		const { user } = await registerUseCase.execute({
+			name: "John Doe",
+			email: "johndoe@example.com",
+			password: "123456",
+		});
+
+		expect(user.id).toEqual(expect.any(String));
+	});
+
 	test("should hash user password upon registration", async () => {
 		const usersRepository = new InMemoryUsersRepository();
 

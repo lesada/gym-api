d55e8cf028f0db71588198d06d6a23fc0acea04a
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2025-05-08 20:34:24 -0300

    feat: authenticate service

--- src/repositories/users-repository.ts
+++ src/repositories/users-repository.ts
@@ -1,4 +1,4 @@
-import type { Prisma, User } from "generated/prisma";
+import type { Prisma, User } from "@prisma/client";
 
 export interface UsersRepository {
 	findByEmail(email: string): Promise<User | null>;
---
+++ src/services/authenticate.ts
@@ -0,0 +1,32 @@
+import type { UsersRepository } from "@/repositories/users-repository";
+import type { User } from "@prisma/client";
+import { compare } from "bcryptjs";
+import { InvalidCredentialsError } from "./errors/invalid-credentials";
+
+interface AuthenticateServiceRequest {
+	email: string;
+	password: string;
+}
+
+type AuthenticateServiceResponse = {
+	user: User;
+};
+
+export class AuthenticateService {
+	constructor(private readonly usersRepository: UsersRepository) {}
+
+	async execute({
+		email,
+		password,
+	}: AuthenticateServiceRequest): Promise<AuthenticateServiceResponse> {
+		const user = await this.usersRepository.findByEmail(email);
+		if (!user) throw new InvalidCredentialsError();
+
+		const doesPasswordMatches = await compare(password, user.password_hash);
+		if (!doesPasswordMatches) throw new InvalidCredentialsError();
+
+		return {
+			user,
+		};
+	}
+}

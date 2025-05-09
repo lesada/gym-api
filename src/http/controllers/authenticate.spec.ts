ba7a4e84081f3e35b794e3816153c089aacea0d0
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2025-05-09 14:55:32 -0300

    test: authenticate controller

---
+++ src/http/controllers/authenticate.spec.ts
@@ -0,0 +1,58 @@
+import { authenticate } from "@/http/controllers/authenticate";
+import { InvalidCredentialsError } from "@/services/errors/invalid-credentials";
+import { makeAuthenticateService } from "@/services/factories/make-authenticate-service";
+import type { FastifyReply, FastifyRequest } from "fastify";
+import { type Mock, beforeEach, describe, expect, it, vi } from "vitest";
+
+vi.mock("@/services/factories/make-authenticate-service", () => ({
+	makeAuthenticateService: vi.fn(),
+}));
+
+describe("authenticate controller", () => {
+	const mockRequest = {
+		body: { email: "user@example.com", password: "validpassword" },
+	} as unknown as FastifyRequest;
+	const mockReply = {
+		status: vi.fn().mockReturnThis(),
+		send: vi.fn(),
+	} as unknown as FastifyReply;
+
+	beforeEach(() => {
+		vi.clearAllMocks();
+	});
+
+	it("should return 200 if credentials are valid", async () => {
+		const mockAuthenticateService = {
+			execute: vi.fn().mockResolvedValue(true),
+		};
+
+		(makeAuthenticateService as Mock).mockReturnValue(mockAuthenticateService);
+
+		await authenticate(mockRequest, mockReply);
+
+		expect(mockReply.status).toHaveBeenCalledWith(200);
+		expect(mockReply.send).toHaveBeenCalled();
+	});
+
+	it("should return 400 if credentials are invalid", async () => {
+		const mockAuthenticateService = {
+			execute: vi.fn().mockRejectedValue(new InvalidCredentialsError()),
+		};
+
+		(makeAuthenticateService as Mock).mockReturnValue(mockAuthenticateService);
+
+		await authenticate(mockRequest, mockReply);
+
+		expect(mockReply.status).toHaveBeenCalledWith(400);
+		expect(mockReply.send).toHaveBeenCalled();
+	});
+
+	it("should return 400 if request body is invalid", async () => {
+		mockRequest.body = { email: "invalid-email", password: "123" };
+
+		await authenticate(mockRequest, mockReply);
+
+		expect(mockReply.status).toHaveBeenCalledWith(400);
+		expect(mockReply.send).toHaveBeenCalled();
+	});
+});
--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -12,20 +12,25 @@ export async function authenticate(
 	request: FastifyRequest,
 	reply: FastifyReply,
 ) {
-	const { email, password } = authenticateBodySchema.parse(request.body);
-
 	try {
+		const { email, password } = authenticateBodySchema.parse(request.body);
 		const authenticateService = makeAuthenticateService();
 		await authenticateService.execute({
 			email,
 			password,
 		});
-	} catch (err) {
-		if (err instanceof InvalidCredentialsError) {
+	} catch (error) {
+		if (error instanceof InvalidCredentialsError) {
 			return reply.status(400).send();
 		}
 
-		throw err;
+		if (error instanceof z.ZodError)
+			return reply.status(400).send({ message: error.flatten().fieldErrors });
+
+		if (error instanceof Error)
+			return reply.status(400).send({ message: error.message });
+
+		throw error;
 	}
 
 	return reply.status(200).send();
